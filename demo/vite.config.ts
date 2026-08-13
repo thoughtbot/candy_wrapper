1091e4a7e5dc236f8c5bde06b362efc3992268b3
Johny Ho <johny@thoughtbot.com>
2026-08-12 22:34:31 -0400

    wip

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -31,18 +31,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@thoughtbot/candy_wrapper': path.resolve(__dirname, '../src/index'),
-      '@/components/ui': path.resolve(
-        __dirname,
-        '../wrappers/ts/shadcn/v4/components/ui'
-      ),
-      '@/registry/bases/radix/lib': path.resolve(
-        __dirname,
-        '../wrappers/ts/shadcn/v4/registry/bases/radix/lib'
-      ),
-      '@/app/(create)/components': path.resolve(
-        __dirname,
-        '../wrappers/ts/shadcn/v4/app/(create)/components'
-      ),
     },
   },
   optimizeDeps: {
